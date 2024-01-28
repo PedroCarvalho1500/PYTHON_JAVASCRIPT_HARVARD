739bee9d20816fe964861f167d013463660037f8
PedroCarvalho1500 <pedro_alves@gec.inatel.br>
2024-01-28 14:12:11 -0300

    tasks.html finished and fixing counter in hello.html

--- JAVASCRIPT/scripts/counter.js
+++ JAVASCRIPT/scripts/counter.js
@@ -1,13 +1,17 @@
-let counter = 0;
+if (!localStorage.getItem('counter')){
+    localStorage.setItem('counter', 0);
+}
 function count(){
     //alert('Hello, world!')
+    let counter = localStorage.getItem('counter');
     counter++;
     console.log(counter);
     //alert(counter);
     //myTitle = document.getElementById("click_here").innerHTML;
     //console.log(myTitle);
     document.querySelector('h1').innerHTML = counter;
-    
+    localStorage.setItem('counter',counter);
+
     if (counter % 10 === 0){
         alert(`Count is now ${counter}`)
     }
@@ -17,6 +21,8 @@ function count(){
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');   
     document.querySelector("button.count_button").onclick = count;
+    //setInterval(count, 1000);
                 //document.querySelector("button.count_button").addEventListener('click', count);
     })
