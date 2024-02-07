c142126504e670e92f997adb6df27c87ebb0333b
PedroCarvalho1500 <pedro_alves@gec.inatel.br>
2024-02-07 19:46:46 -0300

    Implementation of browser tests with python unittest.

---
+++ DJANGO/airline/selenium/tests.py
@@ -0,0 +1,40 @@
+import os
+import pathlib
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def file_uri(filename):
+    return pathlib.Path(os.path.abspath(filename)).as_uri()
+
+
+driver = webdriver.Chrome()
+
+class WebPageTests(unittest.TestCase):
+    def test_title(self):
+        driver.get(file_uri("counter.html"))
+        self.assertEqual(driver.title,"Counter")
+
+    def test_increase(self):
+        driver.get(file_uri("counter.html"))
+        increase = driver.find_element(by=By.ID,value="increase")
+        increase.click()
+        self.assertEqual(driver.find_element(by=By.TAG_NAME,value="h1").text, "1")
+
+    def test_decrease(self):
+        driver.get(file_uri("counter.html"))
+        decrease = driver.find_element(by=By.ID,value="decrease")
+        decrease.click()
+        self.assertEqual(driver.find_element(by=By.TAG_NAME,value="h1").text, "-1")
+
+    def test_multiple_increase(self):
+        driver.get(file_uri("counter.html"))
+        increase = driver.find_element(by=By.ID,value="increase")
+        for i in range(3):
+            increase.click()
+        self.assertEqual(driver.find_element(by=By.TAG_NAME,value="h1").text, "3")
+
+if __name__ == '__main__':
+    unittest.main()
